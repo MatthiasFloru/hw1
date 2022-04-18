e9eaee56e6db4448f4477d10e37dd8ae453f41fe
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-04-18 20:21:37 +0000

    first draft

--- kmdb.sql
+++ kmdb.sql
@@ -100,14 +100,89 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS casts;
+DROP TABLE IF EXISTS actors;
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_title TEXT,
+  year_released TEXT,
+  mpaa_rating TEXT,
+  studio_id INTEGER,
+  cast_id INTEGER
+);
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE casts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  character TEXT,
+  movie_id INTEGER,
+  actor_id INTEGER
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO movies (
+    movie_title,
+    year_released,
+    mpaa_rating,
+  studio_id,
+  cast_id
+)
+VALUES (
+    "Batman Begins",
+    "2005",
+    "PG-13",
+    1,
+    1
+)
+
+-- Batman Begins          2005           PG-13  Warner Bros.
+-- The Dark Knight        2008           PG-13  Warner Bros.
+-- The Dark Knight Rises  2012           PG-13  Warner Bros.
+
+INSERT INTO studios (
+    name
+)
+VALUES (
+    "Warner Bros."
+)
+
+INSERT INTO casts (
+    movie_id,
+    actor_id,
+    character
+)
+VALUES (
+    1,
+    1,
+    "Bruce Wayne"
+)
+
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Christian Bale"
+)
+
+-- Batman Begins          Christian Bale        Bruce Wayne
+-- Batman Begins          Michael Caine         Alfred
+-- Batman Begins          Liam Neeson           Ra's Al Ghul
+-- Batman Begins          Katie Holmes          Rachel Dawes
+-- Batman Begins          Gary Oldman           Commissioner Gordon
 
 -- Prints a header for the movies output
 .print "Movies"
